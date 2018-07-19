000a3d75cc1a6ba3e755a7389a638ab87bb95cfd
JaneWB <brownjannick@att.net>
2018-07-18 20:09:00 -0500

    Area of rectangle

--- CG2-2a/Program.cs
+++ CG2-2a/Program.cs
@@ -6,7 +6,15 @@ namespace CG2_2a
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double length, breadth, area;
+            Console.WriteLine("Enter length of rectangle : ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter breadth of rectangle : ");
+            breadth = Convert.ToDouble(Console.ReadLine());
+            area = length * breadth;
+            Console.WriteLine("\nArea of rectangle: " + area);
+            Console.ReadKey();
         }
     }
 }
